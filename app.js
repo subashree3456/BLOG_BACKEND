c2358cdd7a41f402f838f60362fa2a4301080a9e
subashree3456 <107339951+subashree3456@users.noreply.github.com>
2024-02-24 14:28:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-// app.get('/' ,(req,res)=>{
-//   res.send("Server is running..... ")
-//   })
+app.get('/' ,(req,res)=>{
+  res.send("Server is running..... ")
+  })
 
 mongoose.connect("mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority&appName=Cluster0")
 .then(() => app.listen(9000))
