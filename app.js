b62dd28a40e79a249780501d8130611d611af2cc
subashree3456 <107339951+subashree3456@users.noreply.github.com>
2024-02-24 13:56:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+
+// app.get('/' ,(req,res)=>{
+//   res.send("Server is running..... ")
+//   })
+
+mongoose.connect("mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority&appName=Cluster0")
+.then(() => app.listen(9000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 9000")
+  )
+  .catch((err) => console.log(err));
+
+// new pwd : wUma7KRfrlToEO1H
+
+// feb 07 -> nCwY3E7qwmGv0dlS
+
+// nCwY3E7qwmGv0dlS
+
+//mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/Blog?retryWrites=true&w=majority
+
+
+// mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+
+// mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+
+//mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+// mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+
+// mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+
+
+// mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+// mongodb+srv://admin:nCwY3E7qwmGv0dlS@cluster0.iu8xqoo.mongodb.net/BlogretryWrites=true&w=majority
+
+
+// 
+
---
+++ routes/blog-routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {   getAllBlogs,  addBlog, updateBlog, getById,  deleteBlog, getByUserId,} from "../controllers/blog-controller";
+const blogRouter = express.Router();
+
+// Router.get("/", getAllBlogs);
+
+blogRouter.get("/", getAllBlogs);
+blogRouter.post("/add", addBlog);
+blogRouter.put("/update/:id", updateBlog);
+blogRouter.get("/:id", getById);
+blogRouter.delete("/:id", deleteBlog);
+blogRouter.get("/user/:id", getByUserId);
+
+export default blogRouter;
